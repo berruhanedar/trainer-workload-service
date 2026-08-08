55d7baecb64f976a84d26870311149e8bb594310
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-08-08 13:22:12 +0300

    Implement trainer workload REST endpoint

---
+++ src/main/java/com/berruhanedar/app/service/TrainerWorkloadService.java
@@ -0,0 +1,11 @@
+package com.berruhanedar.app.service;
+
+import com.berruhanedar.app.dto.TrainerWorkloadRequestDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TrainerWorkloadService {
+
+    public void processWorkload(TrainerWorkloadRequestDto request) {
+    }
+}
