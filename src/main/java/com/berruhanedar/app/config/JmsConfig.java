1c11e7f52ce14dfd640cbd8baf51727d1d134d20
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-08-14 21:07:24 +0300

    Configure JMS consumer scaling and error handling

--- src/main/java/com/berruhanedar/app/config/JmsConfig.java
+++ src/main/java/com/berruhanedar/app/config/JmsConfig.java
@@ -1,12 +1,16 @@
 package com.berruhanedar.app.config;
 
-import tools.jackson.databind.json.JsonMapper;
+import jakarta.jms.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.support.converter.JacksonJsonMessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+import tools.jackson.databind.json.JsonMapper;
 
+@Slf4j
 @Configuration
 public class JmsConfig {
 
@@ -16,5 +20,16 @@ public class JmsConfig {
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
         return converter;
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+        factory.setConcurrency("2-5");
+        factory.setErrorHandler(exception ->
+                log.error("Error while processing JMS message", exception));
+        return factory;
     }
 }
