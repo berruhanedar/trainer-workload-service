921601a8efa0521322df964c6aaf0bd5c9acdaae
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-08-14 21:40:29 +0300

    Fix JMS JSON type mapping between microservices

--- src/main/java/com/berruhanedar/app/config/JmsConfig.java
+++ src/main/java/com/berruhanedar/app/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package com.berruhanedar.app.config;
 
+import com.berruhanedar.app.dto.TrainerWorkloadRequestDto;
 import jakarta.jms.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -10,26 +11,50 @@ import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 import tools.jackson.databind.json.JsonMapper;
 
+import java.util.Map;
+
 @Slf4j
 @Configuration
 public class JmsConfig {
 
     @Bean
     public MessageConverter jacksonJmsMessageConverter(JsonMapper jsonMapper) {
-        JacksonJsonMessageConverter converter = new JacksonJsonMessageConverter(jsonMapper);
+
+        JacksonJsonMessageConverter converter =
+                new JacksonJsonMessageConverter(jsonMapper);
+
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
+
+        converter.setTypeIdMappings(
+                Map.of(
+                        "trainerWorkload",
+                        TrainerWorkloadRequestDto.class
+                )
+        );
+
         return converter;
     }
 
     @Bean
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            MessageConverter messageConverter) {
+
+        DefaultJmsListenerContainerFactory factory =
+                new DefaultJmsListenerContainerFactory();
+
         factory.setConnectionFactory(connectionFactory);
         factory.setMessageConverter(messageConverter);
         factory.setConcurrency("2-5");
+
         factory.setErrorHandler(exception ->
-                log.error("Error while processing JMS message", exception));
+                log.error(
+                        "Error while processing JMS message",
+                        exception
+                )
+        );
+
         return factory;
     }
 }
