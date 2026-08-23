139060a964baba4bb9f6eefece9edd3f4a615fd1
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-08-23 21:30:47 +0300

    Update

--- src/test/java/com/berruhanedar/app/TrainerWorkloadServiceTest.java
+++ src/test/java/com/berruhanedar/app/TrainerWorkloadServiceTest.java
@@ -1,4 +1,4 @@
-package com.berruhanedar.app.service;
+package com.berruhanedar.app;
 
 import com.berruhanedar.app.dto.TrainerWorkloadRequestDto;
 import com.berruhanedar.app.entity.MonthSummary;
@@ -7,6 +7,7 @@ import com.berruhanedar.app.entity.YearSummary;
 import com.berruhanedar.app.enums.ActionType;
 import com.berruhanedar.app.mapper.TrainerWorkloadMapper;
 import com.berruhanedar.app.repository.TrainerWorkloadRepository;
+import com.berruhanedar.app.service.TrainerWorkloadService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
