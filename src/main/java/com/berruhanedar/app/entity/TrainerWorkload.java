22a40ab0b7ce2a0cc546a09c641d7d2bd57c763e
Berru Hanedar <115636713+berruhanedar@users.noreply.github.com>
2026-08-23 21:23:07 +0300

    Add compound index for trainer name search

--- src/main/java/com/berruhanedar/app/entity/TrainerWorkload.java
+++ src/main/java/com/berruhanedar/app/entity/TrainerWorkload.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection = "trainer_workloads")
+@CompoundIndex(name = "trainer_name_idx", def = "{'trainerFirstName': 1, 'trainerLastName': 1}")
 @Getter
 @Setter
 @NoArgsConstructor
